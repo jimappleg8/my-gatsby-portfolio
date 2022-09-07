49003b86f4f7561575678046133a264b9eed09ce
Jim Applegate <jimappleg8@gmail.com>
2022-09-07 18:26:21 +0000

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
+    title: `Gatsby Tutorial Website`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`
+      }
+    },
   ],
-}
+};
